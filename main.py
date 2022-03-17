cb1f2e82bf3ddd65f177a15ff6f56ae41c547604
selimcanermis <ermisselimcan@gmail.com>
2022-03-17 12:05:51 +0300

    Footballer value

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def clubCheck(club):
         print("2")
     return club
 
-def getSoup(url, select):
+def getSoup(url, choice):
     html_request = requests.get(url, headers=headers)
     soup = BeautifulSoup(html_request.content, "html.parser")
-    getList(soup, select)
+    getList(soup, choice)
 
-def getList(soup, select):
-    if select == 1:
+def getList(soup, choice):
+    if choice == 1:
         list_club = soup.find("table",{"class":"items"}).find("tbody")
-        count = 0
+        no = 0
 
         #TODO title gelecek
 
@@ -36,47 +36,37 @@ def getList(soup, select):
             club = i.find_all("td")[2].text.strip()
             league = i.find_all("td")[3].text.strip()
             value = i.find_all("td")[4].text.strip()
-            count +=1
+            no +=1
 
             
-            print(str(count).rjust(2), club.ljust(70,"."), league.ljust(30), value.strip().ljust(5))
+            print(str(no).rjust(2), club.ljust(70,"."), league.ljust(30), value.strip().ljust(5))
 
-    if select == 2:
+    if choice == 2:
         global x
         list_footballer = soup.find("table",{"class":"items"}).find("tbody")
-        count = 0
+        no = 0
 
         #TODO title gelecek
 
         for temp_name_value in list_footballer.find_all("td",{"class":"hauptlink"}):
             add1 = temp_name_value.find("a").text.strip()
 
-            count +=1
+            no +=1
             playerList1.append(add1)
 
-        count = 0
-        print(count)
-
-        for temp_age in list_footballer.find_all("tbody"):
-            add2 = temp_age.find_all("tr")[1].text.strip()
-            #add2 = temp_age.find("td")[3]
-            print(add2)
-
-
-            count +=1
-            playerList2.append(add2)
-
+        
+        no = 1
+        value_no = 1
         for name in range(0,50,2):
-            print(playerList1[name])
-
-        for value in range(1,50,2):
-            print(playerList1[value])
-
-        """
-        for age in range(0,50):
-            print(playerList2[age])
-        """
-            
+            x = True
+            if x == True:
+                for value in range(value_no,50,2):
+                    print(str(no).rjust(2), playerList1[name].ljust(70,"."), playerList1[value].ljust(30))
+                    no += 1
+                    x = False
+                    break
+                    #print(playerList1[name])
+            value_no += 2
 
 
 #getSoup(club_value_url, 1)
