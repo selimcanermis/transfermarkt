b488325cdc998a48d5975a63c2c2803db132af06
selimcanermis <ermisselimcan@gmail.com>
2022-03-17 13:20:24 +0300

    Add tr and Add position

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ import numpy as np
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
 club_value_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstemannschaften/marktwertetop"
-footballer_value_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop"
+wr_footballer_all_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop"
+wr_gkp_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Torwart&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=0&kontinent_id=0&yt0=G%C3%B6ster"
+wr_def_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Abwehr&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=0&kontinent_id=0&yt0=G%C3%B6ster"
+wr_mid_val_url ="https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Mittelfeld&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=0&kontinent_id=0&yt0=G%C3%B6ster"
+wr_frwd_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Sturm&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=0&kontinent_id=0&yt0=G%C3%B6ster"
+
+tr_footballer_all_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=alle&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=174&kontinent_id=0&yt0=G%C3%B6ster"
+tr_gkp_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Torwart&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=174&kontinent_id=0&yt0=G%C3%B6ster"
+tr_def_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Abwehr&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=174&kontinent_id=0&yt0=G%C3%B6ster"
+tr_mid_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Mittelfeld&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=174&kontinent_id=0&yt0=G%C3%B6ster"
+tr_frwd_val_url = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Sturm&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=174&kontinent_id=0&yt0=G%C3%B6ster"
+
 
 playerList1 = []
 playerList2 = []
@@ -65,9 +76,20 @@ def getList(soup, choice):
                     no += 1
                     x = False
                     break
-                    #print(playerList1[name])
             value_no += 2
+        print("-"*100)
+        playerList1.clear()
 
 
 #getSoup(club_value_url, 1)
-getSoup(footballer_value_url, 2)
+getSoup(wr_footballer_all_val_url, 2)
+getSoup(wr_gkp_val_url, 2)
+getSoup(wr_def_val_url, 2)
+getSoup(wr_mid_val_url, 2)
+getSoup(wr_frwd_val_url, 2)
+
+getSoup(tr_footballer_all_val_url, 2)
+getSoup(tr_gkp_val_url, 2)
+getSoup(tr_def_val_url, 2)
+getSoup(tr_mid_val_url, 2)
+getSoup(tr_frwd_val_url, 2)
