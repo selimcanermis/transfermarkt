6a626165ef43668c3631dec9302f322879d414c9
selimcanermis <ermisselimcan@gmail.com>
2022-03-19 00:09:40 +0300

    Add transfers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sunau import AUDIO_FILE_ENCODING_ADPCM_G723_3
 import requests
 from bs4 import BeautifulSoup
 import urls
@@ -9,7 +10,7 @@ playerList1 = []
 playerList2 = []
 
 def positionSelect():
-    temp = input("1- Tümü\n2- Kaleciler\n3- Defanslar\n4- Orta Sahalar\n5- Forvetler\n0- Geri\nYour Choice?: ")
+    temp = input("1- Tümü\n2- Kaleciler\n3- Defanslar\n4- Orta Sahalar\n5- Forvetler\n0- Geri\nSeçiminiz: ")
     print("-"*30)
 
     return temp
@@ -38,7 +39,11 @@ def titleScript(param, title_param):
         print("-"*100)
         print("|" + title.rjust(60) + "|".rjust(39," "))
         print("-"*100)
-
+    elif title_param == "transfers":
+        title = param.find("h2",{"class":"content-box-headline"}).string.strip().upper()
+        print("-"*100)
+        print("|" + title.rjust(60) + "|".rjust(39," "))
+        print("-"*100)
 
 def getSoup(url, choice):
     html_request = requests.get(url, headers=headers)
@@ -49,6 +54,8 @@ def getSoup(url, choice):
         getListPlayers(soup)
     elif choice == 3:
         getListNational(soup)
+    elif choice == 4:
+        getListTransfer(soup)
 
 def getListTeams(soup):
     # ? MOST VALUABLE TEAMS
@@ -56,13 +63,12 @@ def getListTeams(soup):
     no = 0
     titleScript(soup, title_param="teams")
 
-    for i in list_club.find_all("tr"):
-        club = i.find_all("td")[2].text.strip()
-        league = i.find_all("td")[3].text.strip()
-        value = i.find_all("td")[4].text.strip()
+    for temp_club in list_club.find_all("tr"):
+        club = temp_club.find_all("td")[2].text.strip()
+        league = temp_club.find_all("td")[3].text.strip()
+        value = temp_club.find_all("td")[4].text.strip()
         no += 1
         print(str(no).rjust(2), club.ljust(50, "."), league.ljust(30), value.strip().ljust(5))
-
 
 def getListPlayers(soup):
     # ? MOST VALUABLE PLAYERS
@@ -90,7 +96,6 @@ def getListPlayers(soup):
     print("-"*100)
     playerList1.clear()
 
-
 def getListNational(soup):
     # ? NATIONAL TEAM VALUES
     nationallist = []
@@ -99,10 +104,10 @@ def getListNational(soup):
     no = 0
     titleScript(soup, title_param="national")
 
-    for j in list_national_team.find_all("tr"):
-        national_name = j.find_all("td")[1].text.strip()
+    for temp_national in list_national_team.find_all("tr"):
+        national_name = temp_national.find_all("td")[1].text.strip()
         nationallist.append(national_name)
-        valuefind = j.find_all("td", {"class": "rechts"})
+        valuefind = temp_national.find_all("td", {"class": "rechts"})
         no += 1
         for v in valuefind:
             value = v.find("b")
@@ -121,7 +126,34 @@ def getListNational(soup):
         value_no += 1
     playerList1.clear()
 
+def getListTransfer(soup):
+    # ? MOST VALUABLE TRANSFERS
+    list_transfer = soup.find("table", {"class": "items"}).find("tbody")
+    no = 0
+    titleScript(soup, title_param="transfers")
 
+    for temp_name_value in list_transfer.find_all("td", {"class": "hauptlink"}):
+        add2 = temp_name_value.find("a").text.strip()
+        no += 1
+        playerList1.append(add2)
+
+    no = 1
+    club_name = 1
+    value_no = 2
+    for name in range(0, 75, 3):
+        x = True
+        if x == True:
+            for club in range(club_name, 75, 3):
+                for value in range(value_no, 75, 3):
+                    print(str(no).rjust(2), playerList1[name].ljust(50, "."), playerList1[club].ljust(30), playerList1[value].strip().ljust(5))
+                    no += 1
+                    value_no += 3
+                    club_name += 3
+                    x = False
+                    break
+                break
+    print("-"*100)
+    playerList1.clear()
 
 def menu():
     info.infoScript()
@@ -132,7 +164,7 @@ def menu():
             print("-"*30)
             print("TRANSFERMARKT VERILERI".rjust(25))
             print("-"*30)
-            first_choice = input("1- En değerli kulüpler\n2- En değerli futbolcular \n3- En değerli milli takımlar \n0- Çıkış\nSeçiminiz?: ")
+            first_choice = input("1- En değerli kulüpler\n2- En değerli futbolcular \n3- En değerli milli takımlar\n4- En pahalı transferler\n0- Çıkış\nSeçiminiz?: ")
             print("-"*30)
 
             if first_choice == "0":
@@ -143,7 +175,7 @@ def menu():
 
         if start == True:
             if first_choice == "1":
-                choice = input("1- Genel en değerli kulüpler\n2- Türkiye\n3- İngiltere\n4- İspanya\n5- Almanya\n6- İtalya\n7- Fransa\n8- Portekiz\n9- Hollanda\n10- Belçika\n0- Geri\nYour Choice?: ")
+                choice = input("1- Genel en değerli kulüpler\n2- Türkiye\n3- İngiltere\n4- İspanya\n5- Almanya\n6- İtalya\n7- Fransa\n8- Portekiz\n9- Hollanda\n10- Belçika\n0- Geri\nSeçiminiz: ")
                 print("-"*30)
                 if choice == "0":
                     start = False
@@ -170,7 +202,7 @@ def menu():
                         getSoup(urls.bel_club_val, 1)  # ! BELÇİKA
 
             elif first_choice == "2":
-                choice = input("1- Genel en değerli futbolcular\n2- Türkiye\n3- İngiltere\n4- İspanya\n5- Almanya\n6- İtalya\n7- Fransa\n8- Portekiz\n9- Arjantin\n10- Brezilya\n0- Geri\nYour Choice?: ")
+                choice = input("1- Genel en değerli futbolcular\n2- Türkiye\n3- İngiltere\n4- İspanya\n5- Almanya\n6- İtalya\n7- Fransa\n8- Portekiz\n9- Arjantin\n10- Brezilya\n0- Geri\nSeçiminiz: ")
                 print("-"*30)
                 if choice == "0":
                     start = False
@@ -319,8 +351,95 @@ def menu():
             elif first_choice == "3": 
                 getSoup(urls.national_team_val, 3)
 
+            elif first_choice == "4":
+                choice = input("1- Genel en pahalı transferler\n2- Uyruklarına göre en pahalı transferler\n3- Sezonlara göre en pahalı transferler\n0- Geri\nSeçiminiz: ")
+                print("-"*30)
+                if choice == "0":
+                    start = False
+                else:
+                    if choice == "1": # ! GENEL
+                        getSoup(urls.wr_transfer_all, 4)
+                    elif choice == "2":
+                        nat_choice = input("1- Türkiye\n2- İngiltere\n3- İspanya\n4- Almanya\n5- İtalya\n6- Fransa\n7- Hollanda\n8- Portekiz\n9- Arjantin\n10- Brezilya\n0- Geri\nSeçiminiz: ")
+                        print("-"*30)
+                        if nat_choice == "0":
+                            break
+                        elif nat_choice == "1": # ! TÜRKİYE
+                            getSoup(urls.tr_transfer_all, 4)
+                        elif nat_choice == "2": # ! İNGİLTERE
+                            getSoup(urls.eng_transfer_all, 4)
+                        elif nat_choice == "3": # ! İSPANYA
+                            getSoup(urls.esp_transfer_all, 4)
+                        elif nat_choice == "4": # ! ALMANYA
+                            getSoup(urls.ger_transfer_all, 4)
+                        elif nat_choice == "5": # ! İTALYA
+                            getSoup(urls.ita_transfer_all, 4)
+                        elif nat_choice == "6": # ! FRANSA
+                            getSoup(urls.fra_transfer_all, 4)
+                        elif nat_choice == "7": # ! HOLLANDA
+                            getSoup(urls.net_transfer_all, 4)
+                        elif nat_choice == "8": # ! PORTEKİZ
+                            getSoup(urls.por_transfer_all, 4)
+                        elif nat_choice == "9": # ! ARJANTİN
+                            getSoup(urls.arg_transfer_all, 4)
+                        elif nat_choice == "10": # ! BREZİLYA
+                            getSoup(urls.bre_transfer_all, 4)
+                    elif choice == "3":
+                        season_choice = input("1-  2022/2023\n2-  2021/2022\n3-  2020/2021\n4-  2019/2020\n5-  2018/2019\n6-  2017/2018\n7-  2016/2017\n8-  2015/2016\n9-  2014/2015\n10- 2013/2014\n11- 2012/2013\n12- 2011/2012\n13- 2010/2011\n14- 2009/2010\n15- 2008/2009\n16- 2010/2008\n17- 2006/2007\n18- 2005/2006\n19- 2004/2005\n20- 2003/2004\n21- 2002/2003\n22- 2001/2002\n23- 2000/2001\n0- Geri\nSeçiminiz: ")
+                        print("-"*30)
+                        if season_choice == "0":
+                            break
+                        elif season_choice == "1": 
+                            getSoup(urls.wr_transfer_22_23, 4)
+                        elif season_choice == "2": 
+                            getSoup(urls.wr_transfer_21_22, 4)
+                        elif season_choice == "3": 
+                            getSoup(urls.wr_transfer_20_21, 4)
+                        elif season_choice == "4": 
+                            getSoup(urls.wr_transfer_19_20, 4)
+                        elif season_choice == "5": 
+                            getSoup(urls.wr_transfer_18_19, 4)
+                        elif season_choice == "6": 
+                            getSoup(urls.wr_transfer_17_18, 4)
+                        elif season_choice == "7": 
+                            getSoup(urls.wr_transfer_16_17, 4)
+                        elif season_choice == "8": 
+                            getSoup(urls.wr_transfer_15_16, 4)
+                        elif season_choice == "9": 
+                            getSoup(urls.wr_transfer_14_15, 4)
+                        elif season_choice == "10": 
+                            getSoup(urls.wr_transfer_13_14, 4)
+                        elif season_choice == "11": 
+                            getSoup(urls.wr_transfer_12_13, 4)
+                        elif season_choice == "12": 
+                            getSoup(urls.wr_transfer_11_12, 4)
+                        elif season_choice == "13": 
+                            getSoup(urls.wr_transfer_10_11, 4)
+                        elif season_choice == "14": 
+                            getSoup(urls.wr_transfer_09_10, 4)
+                        elif season_choice == "15": 
+                            getSoup(urls.wr_transfer_08_09, 4)
+                        elif season_choice == "16": 
+                            getSoup(urls.wr_transfer_07_08, 4)
+                        elif season_choice == "17": 
+                            getSoup(urls.wr_transfer_06_07, 4)
+                        elif season_choice == "18": 
+                            getSoup(urls.wr_transfer_05_06, 4)
+                        elif season_choice == "19": 
+                            getSoup(urls.wr_transfer_04_05, 4)
+                        elif season_choice == "20": 
+                            getSoup(urls.wr_transfer_03_04, 4)
+                        elif season_choice == "21": 
+                            getSoup(urls.wr_transfer_02_03, 4)
+                        elif season_choice == "22": 
+                            getSoup(urls.wr_transfer_01_02, 4)
+                        elif season_choice == "23": 
+                            getSoup(urls.wr_transfer_00_01, 4)
+
+
             start = False
 
 
 
 menu()
+#jgetSoup(urls.most_transfer, 2)
--- urls.py
+++ urls.py
@@ -66,7 +66,7 @@ ita_def_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespi
 ita_mid_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Mittelfeld&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=75&kontinent_id=0&yt0=G%C3%B6ster"
 ita_frwd_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Sturm&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=75&kontinent_id=0&yt0=G%C3%B6ster"
 
-#* FR FOOTBALLER POSITIONS
+#* FRA FOOTBALLER POSITIONS
 fra_footballer_all_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=alle&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=50&kontinent_id=0&yt0=G%C3%B6ster"
 fra_gkp_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Torwart&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=50&kontinent_id=0&yt0=G%C3%B6ster"
 fra_def_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Abwehr&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=50&kontinent_id=0&yt0=G%C3%B6ster"
@@ -93,3 +93,51 @@ bre_gkp_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespi
 bre_def_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Abwehr&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=26&kontinent_id=0&yt0=G%C3%B6ster"
 bre_mid_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Mittelfeld&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=26&kontinent_id=0&yt0=G%C3%B6ster"
 bre_frwd_val = "https://www.transfermarkt.com.tr/spieler-statistik/wertvollstespieler/marktwertetop/plus/0/galerie/0?ausrichtung=Sturm&spielerposition_id=alle&altersklasse=alle&jahrgang=0&land_id=26&kontinent_id=0&yt0=G%C3%B6ster"
+
+
+#* WORLD TRANSFER SEASONS
+wr_transfer_all = "https://www.transfermarkt.com.tr/statistik/transferrekorde"
+wr_transfer_22_23 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2022&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_21_22 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2021&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_20_21 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2020&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_19_20 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2019&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_18_19 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2018&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_17_18 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2017&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_16_17 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2016&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_15_16 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2015&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_14_15 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2014&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_13_14 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2013&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_12_13 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2012&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_11_12 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2011&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_10_11 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2010&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_09_10 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2009&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_08_09 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2008&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_07_08 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2007&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_06_07 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2006&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_05_06 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2005&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_04_05 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2004&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_03_04 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2003&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_02_03 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2002&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_01_02 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2001&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+wr_transfer_00_01 = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2000&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+
+#* TR TRANSFER SEASONS
+tr_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=174&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* ENG TRANSFER SEASONS
+eng_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=189&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* ESP TRANSFER SEASONS
+esp_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=157&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* GER TRANSFER SEASONS
+ger_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=40&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* ITA TRANSFER SEASONS
+ita_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=75&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* FRA TRANSFER SEASONS
+fra_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=50&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* NET TRANSFER SEASONS
+net_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=122&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* POR TRANSFER SEASONS
+por_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=136&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* ARG TRANSFER SEASONS
+arg_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=9&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
+#* BRE TRANSFER SEASONS
+bre_transfer_all = "https://www.transfermarkt.com.tr/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=26&ausrichtung=&spielerposition_id=&altersklasse=&jahrgang=0&leihe=&w_s="
